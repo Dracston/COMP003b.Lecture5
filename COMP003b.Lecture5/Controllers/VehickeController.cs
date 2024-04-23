a1b56ae182573e67af17468c372c425f4a270bed
Dracston <lpj11101@gmail.com>
2024-04-22 19:15:26 -0700

    Edit page aded

--- COMP003b.Lecture5/Controllers/VehickeController.cs
+++ COMP003b.Lecture5/Controllers/VehickeController.cs
@@ -9,10 +9,11 @@ namespace COMP003b.Lecture5.Controllers
     [Route("api/[controller]")]
     public class VehiclesController : Controller
     {//Todo: Create an in-memory list
-        private List<Vehicle> _vehicles =  new List<Vehicle>();
+        private List<Vehicle> _vehicles = new List<Vehicle>();
 
         //TODO: Added default constructor to pre-fill
-        public VehiclesController() {
+        public VehiclesController()
+        {
             _vehicles.Add(new Vehicle { Id = 1, Make = "Toyota", Model = "Corolla", Year = 2018 });
             _vehicles.Add(new Vehicle { Id = 2, Make = "Honda", Model = "Civic", Year = 2019 });
             _vehicles.Add(new Vehicle { Id = 3, Make = "Ford", Model = "Fusion", Year = 2020 });
@@ -30,7 +31,7 @@ namespace COMP003b.Lecture5.Controllers
         [HttpGet("{id}")]
         public ActionResult<Vehicle> GetVehicleById(int id)
         {
-            var vehicle=_vehicles.FirstOrDefault(x => x.Id == id);
+            var vehicle = _vehicles.FirstOrDefault(x => x.Id == id);
             if (vehicle == null)
             {
                 return NotFound();
@@ -38,9 +39,40 @@ namespace COMP003b.Lecture5.Controllers
             return vehicle;
         }
         //Todo: POST (create): api/ vehicle
+        [HttpPost]
+        public ActionResult<Vehicle> CreateVehicle(Vehicle vehicle)
+        {
+            //TODO: Automatically assign Id
+            vehicle.Id = _vehicles.Max(v => v.Id) + 1;
 
+            //TODO: Add it to list
+            _vehicles.Add(vehicle);
+
+            return CreatedAtAction(nameof(GetVehicleById), new { id = vehicle.Id }, vehicle);
+        }
 
         //TODO: Put(update0: api.vehicles
-        //TODO: Delete: Api/vehicle
+        [HttpPut]
+        public ActionResult<Vehicle> UpdateVehicle(int id, Vehicle updatedvehicle)
+        {
+            //Todo: Look for the vehicle by Id
+            var vehicle = _vehicles.Find(v => v.Id == id);
+
+            //TODO: return Bad Request if not found. 
+            if (vehicle == null)
+            {
+                return BadRequest();
+            }
+
+            //Todo: Update vehicle
+            vehicle.Make = updatedvehicle.Make;
+            vehicle.Model = updatedvehicle.Model;
+            vehicle.Year = updatedvehicle.Year;
+            return NoContent();
+
+
+            //TODO: Delete: Api/vehicle
+
+        }
     }
 }
